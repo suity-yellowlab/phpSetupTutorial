8fe949913abe0a055da984ee6372231cbaceac23
Hendrik Nettekoven <hndrknettekoven@gmail.com>
2022-07-08 13:30:15 +0200

    first commit

---
+++ app/public/index.php
@@ -0,0 +1,29 @@
+
+
+<?php 
+if (isset($_POST["inputtext"])){
+    // In diesen Block das Programm
+    $inputtext = $_POST["inputtext"]; // Hier wird die Variable aus dem superglobalen POST, aus der Anfrage, kopiert.
+    echo $inputtext;
+    
+}else { ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            <div class="form-group">
+            <label for="inputtext">Eingabe zum alphabetisch sortieren: </label>
+            <input type="text" id="inputtext" class="form-control" name="inputtext">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Abschicken</button>
+        </form>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
+<?php } ?>
